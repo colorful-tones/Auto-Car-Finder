5accb6e7b7d81441b453f7d6b4544f3d1e9e7584
Damon Cook <colorful-tones@users.noreply.github.com>
2024-07-29 17:49:24 -0400

    PHP spacing cleanup

--- themes/autocarfinder/patterns/search.php
+++ themes/autocarfinder/patterns/search.php
@@ -10,7 +10,7 @@
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"},"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|small"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--large)"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary-accent"}}},"typography":{"fontSize":"1.8rem","fontStyle":"normal","fontWeight":"700"},"spacing":{"margin":{"bottom":"0"}}},"textColor":"primary-accent"} -->
-<h2 class="wp-block-heading has-text-align-center has-primary-accent-color has-text-color has-link-color" style="margin-bottom:0;font-size:1.8rem;font-style:normal;font-weight:700"><?php esc_html_e('Find your next ride...', 'autocarfinder');?></h2>
+<h2 class="wp-block-heading has-text-align-center has-primary-accent-color has-text-color has-link-color" style="margin-bottom:0;font-size:1.8rem;font-style:normal;font-weight:700"><?php esc_html_e( 'Find your next ride...', 'autocarfinder' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search make, model, or type","width":75,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"align":"center","style":{"border":{"radius":{"topLeft":"999px","topRight":"0px","bottomLeft":"999px","bottomRight":"0px"}}},"backgroundColor":"main","fontSize":"base"} /--></div>
